d7fe735f4a5650a1e6e1675f7f0c2ff0f7b6e0be
MacTavish27 <abduhakimovfayzullo@gmail.com>
2026-07-09 16:58:31 +0500

    feat: configure filament-language-switch plugin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,9 @@ use App\Listeners\SendOrderToQueue;
 use App\Listeners\SendStatusNotificationToQueue;
 use App\Models\Order;
 use App\Observers\OrderObserver;
+use BezhanSalleh\LanguageSwitch\Enums\ItemStyle;
+use BezhanSalleh\LanguageSwitch\Enums\TriggerStyle;
+use BezhanSalleh\LanguageSwitch\LanguageSwitch;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use SergiX44\Nutgram\Nutgram;
@@ -27,6 +30,14 @@ class AppServiceProvider extends ServiceProvider
             return;
         }
         self::$booted = true;
+
+        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+            $switch->locales(['en', 'ru', 'uz'])
+                ->modalIcon('heroicon-o-language')
+                ->modalIconColor('primary')
+                ->trigger(style: TriggerStyle::AvatarLabel)
+                ->circular();
+        });
 
         // Events
         Event::listen(OrderPlaced::class, SendOrderToQueue::class);
